4114000e65c860f6cb3b266786284b4a6dae67a8
Lomba8 <jack.james.lomba@gmail.com>
2020-09-30 16:00:46 +0200

    added comments

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var scraper = require('table-scraper');
-import extractDate from 'extract-date';
+//import extractDate from 'extract-date';
 
 var date_ob = new Date()
 let anno_node = date_ob.getMonth() > 6 ? date_ob.getFullYear() : date_ob.getFullYear() - 1;
@@ -16,7 +16,7 @@ scraper
         tableData = tableData.slice(1, tableData.length - 1)
 
         for (let idx = 0; idx < tableData.length; idx++) { // .slice() per togliere il primo lelemnto che è null & l'ulettimo che e di una altra tabella nella pagina 
-            mese = tableData[idx][0]["0"].substring(0, 9).split(' ')[0] // 9 perche il mese piu lungo (Settembre) ha 9 ltere
+            mese = tableData[idx][0]["0"].substring(0, 9).split(' ')[0] // 9 perche il mese piu lungo (Settembre) ha 9 lettere
                 //console.log(mese)
 
             if (idx < 4) { anno = anno_node.toString() } else { anno = (anno_node + 1).toString() }
@@ -30,7 +30,7 @@ scraper
                     //console.log(anno + ' ' + mese + ' ' + giorno)
 
                 try {
-                    date.push(extractDate(data, { locale: 'it' })[0].date)
+                    // date.push(extractDate(data, { locale: 'it' })[0].date)
                 } catch (error) {
 
                 }
@@ -38,10 +38,13 @@ scraper
             }
         };
 
-        date.push(date_ob.getMonth() > 6 ? `${date_ob.getFullYear() + 1}-05-21` : `${date_ob.getFullYear()}-05-21`)
+        date.push(date_ob.getMonth() > 6 ? `${date_ob.getFullYear() + 1}-05-21` : `${date_ob.getFullYear()}-05-21`) // Santo Patrono
 
         date.sort();
 
-        console.log(date.slice(1, date.length - 2)) // .slice() per torgliere il giorno di inizio scuola e i due giorni di fine scuola (superiore & infanzia)
+
+        date.shift() //per torgliere il giorno di inizio scuola
+        date.pop() //per torgliere penultimo giorno della tabella (fine scuola superiore)
+        date.pop().slice() //per torgliere ultimo giorno della tabella (fine scuola infanzia)
 
     });
